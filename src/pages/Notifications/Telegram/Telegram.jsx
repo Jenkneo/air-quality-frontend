876a7b62ff61fc5d276893c2499ef556321f253f
Evgeny <jenkneo.lal@gmail.com>
2024-11-06 00:15:11 +0400

    config is done

--- src/pages/Notifications/Telegram/Telegram.jsx
+++ src/pages/Notifications/Telegram/Telegram.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Telegram.css';
 import { getCache } from '../../../utils/cache';
+import { TELEGRAM_BOT_USERNAME, TELEGRAM_URL } from '../../../config';
 
 const GEOLOCATION_KEY = 'geolocation';
-const TELEGRAM_BOT_USERNAME = 'air_pollution_notifications_bot';
-const TELEGRAM_URL = `https://t.me/${TELEGRAM_BOT_USERNAME}`;
+const TELEGRAM_BOT_URL = `${TELEGRAM_URL}/${TELEGRAM_BOT_USERNAME}`;
 
 const Telegram = () => {
   const redirectToTelegram = () => {
     const location = getCache(GEOLOCATION_KEY)
     const lon = String(location.lon).replace('.', '-');
     const lat = String(location.lat).replace('.', '-');
-    const url = `${TELEGRAM_URL}?start=lon${lon}lat${lat}`
+    const url = `${TELEGRAM_BOT_URL}?start=lon${lon}lat${lat}`
 
     window.open(url, '_blank', 'noopener,noreferrer');
   };
