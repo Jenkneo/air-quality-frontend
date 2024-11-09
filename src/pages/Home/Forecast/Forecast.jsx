14bab6da40e04e55d8917e844380ed6172249be2
Evgeny <jenkneo.lal@gmail.com>
2024-11-10 03:17:08 +0400

    home page 24 hour widget

--- src/pages/Home/Forecast/Forecast.jsx
+++ src/pages/Home/Forecast/Forecast.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Forecast.css';
 
-const Forecast = ({ forecastGroupedByDay }) => {
-  if (!forecastGroupedByDay) {
+const Forecast = ({ forecastData }) => {
+  if (!forecastData) {
     return (
       <div className="forecast-container">
         <h3>Данные прогноза недоступны или все еще подгружаются....</h3>
@@ -11,26 +11,116 @@ const Forecast = ({ forecastGroupedByDay }) => {
     )
   }
 
+  const dayForecast = forecastData.list.slice(0, 24);
+
+  function groupByDayAndTimeOfDay(data) {
+    const groupedData = { today: { Morning: [], Day: [], Evening: [], Night: [] }, tomorrow: { Morning: [], Day: [], Evening: [], Night: [] } };
+
+    const currentDate = new Date();
+    const timezoneOffset = currentDate.getTimezoneOffset() * 60 * 1000;
+
+    const today = new Date(currentDate.getTime() - timezoneOffset).toISOString().split('T')[0];
+    const tomorrow = new Date(currentDate.setDate(currentDate.getDate() + 1) - timezoneOffset).toISOString().split('T')[0];
+
+    data.forEach(entry => {
+        const timestamp = entry.dt * 1000;
+        const date = new Date(timestamp - timezoneOffset);
+        
+        const day = date.toISOString().split('T')[0];
+        const hours = date.getHours();
+
+        const timeOfDay = 
+            hours >= 4 && hours < 10 ? "Morning" : 
+            hours >= 10 && hours < 16 ? "Day" : 
+            hours >= 16 && hours < 22 ? "Evening" : 
+            "Night";
+
+        if (day === today) {
+            groupedData.today[timeOfDay].push(entry);
+        } else if (day === tomorrow) {
+            groupedData.tomorrow[timeOfDay].push(entry);
+        }
+    });
+
+    return groupedData;
+  }
+
+  function summarizeData(data) {
+    const groupedData = { today: { Morning: [], Day: [], Evening: [], Night: [] }, tomorrow: { Morning: [], Day: [], Evening: [], Night: [] } };
+    for (const day in data) {
+      for (const timeOfDay in data[day]) {
+        const entries = data[day][timeOfDay];
+        const numEntries = entries.length;
+        if (numEntries === 0) {
+          delete groupedData[day][timeOfDay];
+          continue;
+        }
+
+        const minAQI = Math.min(...entries.map(entry => entry.main.aqi));
+        const maxAQI = Math.max(...entries.map(entry => entry.main.aqi));
+
+        const range = minAQI === maxAQI ? String(minAQI) : `${minAQI}-${maxAQI}`;
+        groupedData[day][timeOfDay] = range;
+      }
+
+      if (Object.keys(groupedData[day]).length === 0) {
+        delete groupedData[day];
+      }
+    }
+
+    return groupedData;
+  }
+
+
+  const data = summarizeData(groupByDayAndTimeOfDay(dayForecast))
+
+
   return (
-    <div className="forecast-container">
-      <h3>Прогноз на следующие сутки по часам</h3>
-      {Object.keys(forecastGroupedByDay).map((day) => (
-        <div className="forecast-day" key={day}>
-          <h4>{day}</h4>
-          {forecastGroupedByDay[day].map((hourData) => (
-            <div className="forecast-hour" key={hourData.dt}>
-              <p>Время: {new Date(hourData.dt * 1000).toLocaleTimeString()}</p>
-              <p>Индекс качества воздуха: {hourData.main.aqi} (AQI)</p>
-              <p>CO: {hourData.components.co} µg/m³</p>
-              <p>NO2: {hourData.components.no2} µg/m³</p>
-              <p>O3: {hourData.components.o3} µg/m³</p>
-              <p>SO2: {hourData.components.so2} µg/m³</p>
-              <p>PM10: {hourData.components.pm10} µg/m³</p>
-              <p>PM2.5: {hourData.components.pm2_5} µg/m³</p>
-            </div>
-          ))}
+    <div className="day-forecast__widget">
+      <div className="day-forecast__widget-row">
+        <span className="day-forecast__time">Утро</span>
+        <div className="day-forecast__current-aqi">
+          <span className="day-forecast__icon">☁️</span>
+          <span className="day-forecast__aqi">1-2 AQI</span>
         </div>
-      ))}
+        <span className="day-forecast__status normal">Показатели в норме</span>
+      </div>
+      <div className="day-forecast__widget-row">
+        <span className="day-forecast__time">День</span>
+        <div className="day-forecast__current-aqi">
+          <span className="day-forecast__icon">☁️</span>
+          <span className="day-forecast__aqi">2-3 AQI</span>
+        </div>
+        <span className="day-forecast__status moderate">Есть загрязнение</span>
+      </div>
+      <div className="day-forecast__widget-row">
+        <span className="day-forecast__time">Вечер</span>
+        <div className="day-forecast__current-aqi">
+          <span className="day-forecast__icon">☁️</span>
+          <span className="day-forecast__aqi">3-5 AQI</span>
+        </div>
+        <span className="day-forecast__status high">Превышение нормы</span>
+      </div>
+      <div className="day-forecast__widget-row">
+        <span className="day-forecast__time">Ночь</span>
+        <div className="day-forecast__current-aqi">
+          <span className="day-forecast__icon">☁️</span>
+          <span className="day-forecast__aqi">2-4 AQI</span>
+        </div>
+        <span className="day-forecast__status normal">Показатели в норме</span>
+      </div>
+      <div className="day-forecast__divider">
+          <div className="day-forecast__bottom-divider" />
+          <div className="day-forecast__after-divider" />
+        </div>
+      <div className="day-forecast__widget-row">
+        <span className="day-forecast__time">Утро</span>
+        <div className="day-forecast__current-aqi">
+          <span className="day-forecast__icon">☁️</span>
+          <span className="day-forecast__aqi">2-4 AQI</span>
+        </div>
+        <span className="day-forecast__status normal">Показатели в норме</span>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,42 +28,11 @@ const Home = () => {
     fetchAirData();
   }, [location]);
 
-  const getNextDayForecast = () => {
-    if (!forecastData) return null;
-    const currentTime = Date.now();
-    const nextDayTimestamp = currentTime + 24 * 60 * 60 * 1000;
-
-    // Фильтруем данные прогноза на следующие 24 часа
-    return forecastData.list.filter(
-      (item) => item.dt * 1000 >= currentTime && item.dt * 1000 <= nextDayTimestamp
-    );
-  };
-
-  const groupByDay = (forecast) => {
-    const days = {};
-
-    forecast.forEach((item) => {
-      const date = new Date(item.dt * 1000);
-      const day = date.toLocaleDateString(); // Получаем дату в формате дня
-
-      if (!days[day]) {
-        days[day] = [];
-      }
-
-      days[day].push(item);
-    });
-
-    return days;
-  };
-
-  const nextDayForecast = getNextDayForecast();
-  const forecastGroupedByDay = nextDayForecast ? groupByDay(nextDayForecast) : null;
-
   return (
     <div className="home-container">
       <h2 className="title">Сегодня</h2>
       <AirQuality airData={airData} />
-      <Forecast forecastGroupedByDay={forecastGroupedByDay} />
+      <Forecast forecastData={forecastData} />
     </div>
   );
 };
